80cdec9ad43fa899c14b2566836559e62d2ae20c
Dzmitry Yermalovich <ermolovichdima22@gmail.com>
2021-02-23 13:53:20 +0300

    edit program,

--- lab1/Lab1test/Lab1test/Source.c
+++ lab1/Lab1test/Lab1test/Source.c
@@ -1,7 +1,5 @@
-#include <iostream>
 #include<math.h>
 #include<stdio.h>
-using namespace std;
 
 int main()
 {
@@ -9,38 +7,38 @@ int main()
 	long long int n;
 	printf("n = ");
 	scanf_s("%lld", &n);
-	long long int currantValue  = 2;
+	long long int currantValue = 2;
 	long long int newValue = 2;
 	int metka = 0;
-	
+
 	for (int index = 2; index <= 1000; index++)
 	{
 		newValue = pow(2, index) - 1;
 
-	for (long long int j = 2; j <= (newValue /2); j++)
-	{
-		if (newValue % j == 0)
+		for (long long int j = 2; j <= (newValue / 2); j++)
 		{
-			metka = 1;
-			break;
+			if (newValue % j == 0)
+			{
+				metka = 1;
+				break;
+			}
 		}
-	}
 
-	
-		if (metka!=1) 
+
+		if (metka != 1)
 		{
 			currantValue = pow(2, (index - 1)) * (pow(2, index) - 1);
 
-			if (n >= currantValue) 
+			if (n >= currantValue)
 			{
 				printf("%lld\n", currantValue);
 			}
-			else 
+			else
 			{
 				break;
 			}
 		}
- 
+
 
 
 		metka = 0;
